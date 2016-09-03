2f63a2cbee79e7809fc307913986569289fcdb5c
DRY411S <DRY411S@users.noreply.github.com>
2016-09-03 10:08:28 +0100

    v0.13.2
    Fix: https://github.com/DRY411S/Add-Assembler-Batteries/issues/3

--- prototypes/recipes.lua
+++ prototypes/recipes.lua
@@ -6,12 +6,7 @@ data:extend(
     category = "crafting-with-fluid",
     energy_required = 5,
     enabled = false,
-    ingredients =
-    {
-      {type="fluid", name="sulfuric-acid", amount=2},
-      {"iron-plate", 1},
-      {"copper-plate", 1}
-    },
+	ingredients = data.raw.recipe["battery"].ingredients,
     result= "battery"
   } 
 
