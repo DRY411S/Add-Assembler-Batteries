3705636290cf8feaf7a78456436d369503bb2edf
DRY411S <DRY411S@users.noreply.github.com>
2017-04-29 06:00:19 +0100

    v0.15.1
    Updated for v0.15.x of factorio
    First 0.15 version

--- prototypes/recipes.lua
+++ prototypes/recipes.lua
@@ -1,13 +1,18 @@
 data:extend(
 {
-  {
+-- Adapted data\base\prototypes\recipe\recipe.lua
+ {
     type = "recipe",
     name = "assembler-battery",
     category = "crafting-with-fluid",
-    energy_required = 5,
-    enabled = false,
-	ingredients = data.raw.recipe["battery"].ingredients,
-    result= "battery"
-  } 
+    normal = data.raw.recipe["battery"].normal,
+    expensive = data.raw.recipe["battery"].expensive,
+    crafting_machine_tint =
+    {
+      primary = {r = 0.970, g = 0.611, b = 0.000, a = 0.000}, -- #f79b0000
+      secondary = {r = 0.000, g = 0.680, b = 0.894, a = 0.357}, -- #00ade45b
+      tertiary = {r = 0.430, g = 0.805, b = 0.726, a = 0.000}, -- #6dcdb900
+    }
+  },
 
 })
