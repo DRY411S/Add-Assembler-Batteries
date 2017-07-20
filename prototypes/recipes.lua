52e288acfef491799acdb049b37d6f6cb25d8047
DRY411S <DRY411S@users.noreply.github.com>
2017-07-20 18:38:06 +0100

    v0.14.3
    Fix: https://github.com/DRY411S/Remove-Chemical-Batteries/issues/3

--- prototypes/recipes.lua
+++ prototypes/recipes.lua
@@ -1,13 +1,14 @@
-data:extend(
-{
-  {
-    type = "recipe",
-    name = "assembler-battery",
-    category = "crafting-with-fluid",
-    energy_required = 5,
-    enabled = false,
-	ingredients = data.raw.recipe["battery"].ingredients,
-    result= "battery"
-  } 
+-- Create an assembler-battery prototype from the vanilla version
+-- It's identical, apart from the crafting category and name
+-- table.deepcopy makes a copy instead of just assigning a pointer
+local newbattery = table.deepcopy(data.raw.recipe.battery)
+newbattery.category = "crafting-with-fluid"
+newbattery.name =  "assembler-battery"
+data.raw.recipe["assembler-battery"] = newbattery
 
-})
+-- Is RemoveChemicalBatteries active?
+-- If not, we may need to unhide the vanilla battery recipe
+-- No need to test whether it's hidden, just unhide it anyway
+if RemoveChemicalBatteries == nil then
+    data.raw.recipe.battery.hidden = false
+end
